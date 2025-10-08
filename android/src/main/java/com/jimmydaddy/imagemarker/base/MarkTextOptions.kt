bbe101540b27f876d46e0074ca5c4e9aa2ec3bb3
robyplopeanu84 <robert.plopeanu@remote-tech.co.uk>
2025-10-08 14:46:31 +0300

    Update MarkTextOptions.kt

--- android/src/main/java/com/jimmydaddy/imagemarker/base/MarkTextOptions.kt
+++ android/src/main/java/com/jimmydaddy/imagemarker/base/MarkTextOptions.kt
@@ -1,33 +1,27 @@
 package com.jimmydaddy.imagemarker.base
-
 import com.facebook.react.bridge.Promise
 import com.facebook.react.bridge.ReadableMap
-
 class MarkTextOptions(options: ReadableMap) : Options(options) {
   lateinit var watermarkTexts: Array<TextOptions?>
-
   init {
     val waterMarkTextsMap = options.getArray("watermarkTexts")
-    if (waterMarkTextsMap != null && waterMarkTextsMap.size() > 0) {
+    if (waterMarkTextsMap!!.size() > 0) {
       watermarkTexts = arrayOfNulls(waterMarkTextsMap.size())
       for (i in 0 until waterMarkTextsMap.size()) {
-        val textMap = waterMarkTextsMap.getMap(i)
-        if (textMap != null) {
-          watermarkTexts[i] = TextOptions(textMap)
-        }
+        val textMap = waterMarkTextsMap.getMap(i)!!
+        watermarkTexts[i] = TextOptions(textMap)
       }
     }
   }
-
   companion object {
     @JvmStatic
     fun checkParams(opts: ReadableMap, promise: Promise): MarkTextOptions? {
-      return try {
-        MarkTextOptions(opts)
+      try {
+        return MarkTextOptions(opts)
       } catch (e: MarkerError) {
         promise.reject(e.getErrorCode(), e.getErrMsg())
-        null
       }
+      return null
     }
   }
 }
